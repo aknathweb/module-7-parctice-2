c562de805da3c6c1a908e89e76cf003b577bdb8f
aknathweb <aniknwu.cse@gmail.com>
2022-09-13 14:16:47 +0600

    Issue Tracker probably all bug solve

--- main.js
+++ main.js
@@ -5,34 +5,35 @@ function submitIssue(e) {
   const description = getInputValue('issueDescription');
   const severity = getInputValue('issueSeverity');
   const assignedTo = getInputValue('issueAssignedTo');
-  const id = Math.floor(Math.random()*100000000) + '';
+  const id = Math.floor(Math.random() * 100000000) + '';
   const status = 'Open';
 
   const issue = { id, description, severity, assignedTo, status };
   let issues = [];
-  if (localStorage.getItem('issues')){
+  if (localStorage.getItem('issues')) {
     issues = JSON.parse(localStorage.getItem('issues'));
   }
   issues.push(issue);
   localStorage.setItem('issues', JSON.stringify(issues));
 
   document.getElementById('issueInputForm').reset();
-  fetchIssues();
   e.preventDefault();
+  fetchIssues();
 }
 
-const closeIssue = id => {
+const setStatusClosed = (id) => {
   const issues = JSON.parse(localStorage.getItem('issues'));
-  const currentIssue = issues.find(issue => issue.id === id);
-  currentIssue.status = 'Closed';
+  const currentIssue = issues.find(issue => issue.id == id);
+  currentIssue.status = currentIssue.status == 'Closed' ? 'Open' : 'Closed';
   localStorage.setItem('issues', JSON.stringify(issues));
   fetchIssues();
 }
 
 const deleteIssue = id => {
   const issues = JSON.parse(localStorage.getItem('issues'));
-  const remainingIssues = issues.filter( issue.id !== id )
+  const remainingIssues = issues.filter(issue => issue.id != id);
   localStorage.setItem('issues', JSON.stringify(remainingIssues));
+  fetchIssues();
 }
 
 const fetchIssues = () => {
@@ -41,16 +42,16 @@ const fetchIssues = () => {
   issuesList.innerHTML = '';
 
   for (var i = 0; i < issues.length; i++) {
-    const {id, description, severity, assignedTo, status} = issues[i];
+    const { id, description, severity, assignedTo, status } = issues[i];
 
-    issuesList.innerHTML +=   `<div class="well">
+    issuesList.innerHTML += `<div class="well">
                               <h6>Issue ID: ${id} </h6>
                               <p><span class="label label-info"> ${status} </span></p>
                               <h3> ${description} </h3>
                               <p><span class="glyphicon glyphicon-time"></span> ${severity}</p>
                               <p><span class="glyphicon glyphicon-user"></span> ${assignedTo}</p>
-                              <a href="#" onclick="setStatusClosed(${id})" class="btn btn-warning">Close</a>
-                              <a href="#" onclick="deleteIssue(${id})" class="btn btn-danger">Delete</a>
+                              <a onclick="setStatusClosed(${id})" class="btn btn-warning">${status === 'Open' ? 'Closed' : 'Open'}</a>
+                              <a  onclick="deleteIssue(${id})" class="btn btn-danger">Delete</a>
                               </div>`;
   }
 }
